2625d576cfcf7892d634247c1d4b8f182ac9983f
Denys Piuro <denys.pyuro@gmail.com>
2024-07-25 17:47:11 +0300

    Fixed

--- app/main.py
+++ app/main.py
@@ -1,22 +1,38 @@
 import math
 from typing import Union
 
+
 class Vector:
     def __init__(self, x: float, y: float) -> None:
         self.x = round(x, 2)
         self.y = round(y, 2)
 
-    def __add__(self, other: 'Vector') -> 'Vector':
-        return Vector(self.x + other.x, self.y + other.y)
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(
+            self.x + other.x,
+            self.y + other.y
+        )
 
-    def __sub__(self, other: 'Vector') -> 'Vector':
-        return Vector(self.x - other.x, self.y - other.y)
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(
+            self.x - other.x,
+            self.y - other.y
+        )
 
-    def __mul__(self, other: Union[int, float, 'Vector']) -> Union[float, 'Vector']:
+    def __mul__(
+        self,
+        other: Union[int, float, "Vector"]
+    ) -> Union[float, "Vector"]:
         if isinstance(other, (int, float)):
-            return Vector(self.x * other, self.y * other)
+            return Vector(
+                self.x * other,
+                self.y * other
+            )
         elif isinstance(other, Vector):
-            return self.x * other.x + self.y * other.y
+            return (
+                self.x * other.x
+                + self.y * other.y
+            )
         else:
             return NotImplemented
 
@@ -25,19 +41,22 @@ class Vector:
         cls,
         start_point: tuple[float, float],
         end_point: tuple[float, float]
-    ) -> 'Vector':
-        return cls(end_point[0] - start_point[0], end_point[1] - start_point[1])
+    ) -> "Vector":
+        return cls(
+            end_point[0] - start_point[0],
+            end_point[1] - start_point[1]
+        )
 
     def get_length(self) -> float:
         return math.sqrt(self.x ** 2 + self.y ** 2)
 
-    def get_normalized(self) -> 'Vector':
+    def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
             return Vector(0, 0)
         return Vector(self.x / length, self.y / length)
 
-    def angle_between(self, other: 'Vector') -> int:
+    def angle_between(self, other: "Vector") -> int:
         dot_product = self * other
         len_self = self.get_length()
         len_other = other.get_length()
@@ -49,10 +68,14 @@ class Vector:
     def get_angle(self) -> int:
         return self.angle_between(Vector(0, 1))
 
-    def rotate(self, degrees: int) -> 'Vector':
+    def rotate(self, degrees: int) -> "Vector":
         radians = math.radians(degrees)
         cos_theta = math.cos(radians)
         sin_theta = math.sin(radians)
-        new_x = self.x * cos_theta - self.y * sin_theta
-        new_y = self.x * sin_theta + self.y * cos_theta
+        new_x = (
+            self.x * cos_theta - self.y * sin_theta
+        )
+        new_y = (
+            self.x * sin_theta + self.y * cos_theta
+        )
         return Vector(new_x, new_y)
