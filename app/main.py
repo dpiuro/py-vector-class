9d1ef68ac53e6cdc3ec7e8fd4eb60581c908ba9c
Denys Piuro <denys.pyuro@gmail.com>
2024-08-02 18:09:32 +0300

    Sol

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from typing import Union
 
 
 class Vector:
-<<<<<<< HEAD
     def __init__(self, coord_x: float, coord_y: float) -> None:
         self.x = round(coord_x, 2)
         self.y = round(coord_y, 2)
@@ -21,38 +20,6 @@ class Vector:
             return Vector(self.x * other, self.y * other)
         elif isinstance(other, Vector):
             return self.x * other.x + self.y * other.y
-=======
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
-
-    def __add__(self, other: "Vector") -> "Vector":
-        return Vector(
-            self.x + other.x,
-            self.y + other.y
-        )
-
-    def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(
-            self.x - other.x,
-            self.y - other.y
-        )
-
-    def __mul__(
-        self,
-        other: Union[int, float, "Vector"]
-    ) -> Union[float, "Vector"]:
-        if isinstance(other, (int, float)):
-            return Vector(
-                self.x * other,
-                self.y * other
-            )
-        elif isinstance(other, Vector):
-            return (
-                self.x * other.x
-                + self.y * other.y
-            )
->>>>>>> 2625d576cfcf7892d634247c1d4b8f182ac9983f
         else:
             return NotImplemented
 
@@ -63,12 +30,7 @@ class Vector:
         end_point: tuple[float, float]
     ) -> "Vector":
         return cls(
-<<<<<<< HEAD
             end_point[0] - start_point[0], end_point[1] - start_point[1]
-=======
-            end_point[0] - start_point[0],
-            end_point[1] - start_point[1]
->>>>>>> 2625d576cfcf7892d634247c1d4b8f182ac9983f
         )
 
     def get_length(self) -> float:
@@ -96,15 +58,6 @@ class Vector:
         radians = math.radians(degrees)
         cos_theta = math.cos(radians)
         sin_theta = math.sin(radians)
-<<<<<<< HEAD
         new_x = self.x * cos_theta - self.y * sin_theta
         new_y = self.x * sin_theta + self.y * cos_theta
-=======
-        new_x = (
-            self.x * cos_theta - self.y * sin_theta
-        )
-        new_y = (
-            self.x * sin_theta + self.y * cos_theta
-        )
->>>>>>> 2625d576cfcf7892d634247c1d4b8f182ac9983f
         return Vector(new_x, new_y)
